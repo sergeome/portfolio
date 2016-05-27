d35a1fca77e7f0a90ea1093cf5376c4ecd3d6520
sergeome <sergeome@gmail.com>
2016-05-27 16:34:19 +0300

    Big changes to the sections.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,35 @@
-//Timeline animation
 $(document).ready(function () {
-    (function () {
-        'use strict';
-// define variables
-        var items = document.querySelectorAll(".timeline li");
 
-// check if an element is in viewport
-// http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-        function isElementInViewport(el) {
-            var rect = el.getBoundingClientRect();
-            return (
-                rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-            );
-        }
+    //Timeline animation
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+    var timelineItems = document.querySelectorAll(".timeline li");
+    var skillList = document.getElementById("skills-list");
 
-        function callbackFunc() {
-            for (var i = 0; i < items.length; i++) {
-                if (isElementInViewport(items[i])) {
-                    items[i].classList.add("in-view");
-                }
+    // code for the isElementInViewport function
+    function callbackFunc() {
+        for (var i = 0; i < timelineItems.length; i++) {
+            if (isElementInViewport(timelineItems[i])) {
+                timelineItems[i].classList.add("in-view");
             }
         }
 
-        // listen for events
-        window.addEventListener("load", callbackFunc);
-        window.addEventListener("resize", callbackFunc);
-        window.addEventListener("scroll", callbackFunc);
+        if (isElementInViewport(skillList)) {
+            $('.loader-bar').each(function () {
+                $(this).addClass("loaded").css("width", $(this).attr("data-percent"));
+            });
+        }
+    }
 
-    })();
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
 });
 
 //Mobile menu toggle
